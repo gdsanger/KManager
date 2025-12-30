56ecbf0deb216204de16a6d5ef0cd812ffbd3943
Christian <ca@perculasoft.de>
2025-12-30 22:45:48 +0100

    ads

--- kmanager/settings.py
+++ kmanager/settings.py
@@ -143,11 +143,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',
-]
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_URL = "/static/"
+STATIC_ROOT = "/opt/KManager/static/"
+
+
 
 # Document storage configuration
 # Documents are stored in filesystem under <APP_ROOT>/data/vermietung/
