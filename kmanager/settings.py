0754cc1b2cfab43a7253ea533807ae6d47bf55dc
gdsanger <ca@perculasoft.de>
2026-04-01 15:57:29 +0200

    asd

--- kmanager/settings.py
+++ kmanager/settings.py
@@ -37,7 +37,8 @@ ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,app.ebner-vermie
 CSRF_TRUSTED_ORIGINS = [
     "https://app.ebner-vermietung.de",
     "http://localhost",
-    "https://116.202.48.216"
+    "https://116.202.48.216",
+    "https://app.angermeier.net:8443"
     # optional Wildcard, falls mehrere Subdomains:
     # "https://*.angermeier.net",
 ]
